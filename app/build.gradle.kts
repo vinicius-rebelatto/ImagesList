17032fd9fe65596ff48b4478b17c878502e9d957
vinicius-rebelatto <vinicius-rebelatto@hotmail.com>
2025-02-14 19:20:40 -0300

    graddle and project settings updated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,6 +60,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.appcompat)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -83,4 +84,5 @@ dependencies {
 
     // Coroutines (optional, for asynchronous programming)
     implementation (libs.kotlinx.coroutines.android)
+    implementation (libs.glide)
 }
